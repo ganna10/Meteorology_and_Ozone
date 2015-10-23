e551466055c27c2c12117ea1f250f11a7e3a2eb1
ganna10 <janecoates10@gmail.com>
2015-10-23 12:57:47 +0200

    Isoprene emissions with T

---
+++ MEGAN/C5H8_emissions_with_T.R
@@ -0,0 +1,19 @@
+# plot isoprene emissions with T
+# Version 0:Jane Coates 23/10/2015
+
+d = read.csv("MEGAN_c5h8_emissions_with_T.csv")
+d = d %>% mutate(Temperature.C = Temperature - 273)
+
+p = ggplot(d, aes(x = Temperature.C, y = C5H8.Emissions))
+p = p + geom_point()
+p = p + geom_line()
+p = p + ylab("Isoprene Emissions (molecules cm-2 s-1)") + xlab(expression(bold(paste("Temperature (", degree, "C)"))))
+p = p + theme_tufte()
+p = p + theme(axis.line = element_line(colour = "black"))
+p = p + theme(axis.title = element_text(face = "bold"))
+p = p + scale_x_continuous(expand = c(0, 0.2))
+p = p + scale_y_continuous(expand = c(0, 1e10))
+
+CairoPDF(file = "isoprene_emissions.pdf", width = 10, height = 7)
+print(p)
+dev.off()
